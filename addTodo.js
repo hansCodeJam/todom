d32fb0a4ebe77ae4afecae9549467ee9f7bab4de
Hans Vos <hans.vos@codeimmersives.com>
2019-11-25 14:33:16 -0500

    done

---
+++ addTodo.js
@@ -0,0 +1,15 @@
+const printValue = function(value) {
+    const listItem = document.createElement('li');
+    listItem.innerText = value;
+    const list = document.querySelector('.todo-list')
+    list.appendChild(listItem);
+  }
+
+
+
+document.querySelector('.add-todo').addEventListener('click', function() {
+    const userInput =document.querySelector('.todo-input').value;
+  
+    printValue(userInput);
+});
+
---
+++ removeTodo.js
@@ -0,0 +1,14 @@
+const removeValue = function(value){
+    const list = document.querySelector('.todo-list')
+    list.removeChild(list.childNodes[value])
+} 
+
+
+
+document.querySelector('.remove-todo').addEventListener('click', function() {
+    const indexInput = document.querySelector('.index-input').value;
+    removeValue(indexInput);
+}); 
+
+
+index-input
