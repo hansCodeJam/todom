f8181f3a437414fbd39d2bf5d2c509d8a56acb39
Hans Vos <hans.vos@codeimmersives.com>
2019-11-25 20:29:56 -0500

    finished

--- addTodo.js
+++ addTodo.js
@@ -6,7 +6,7 @@ const printValue = function(value) {
   }
 
 document.querySelector('.add-todo').addEventListener('click', function() {
-    const userInput =document.querySelector('.todo-input').value;
+    const userInput = document.querySelector('.todo-input').value;
     printValue(userInput);
 });
 
